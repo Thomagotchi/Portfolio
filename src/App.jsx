6b9b0a74eda3474e07157e99ad6031fec6a2c682
Thomagotchi <thomassano87@gmail.com>
2024-08-30 15:47:51 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Portfolio</h1>
+      <p>First commit</p>
+    </>
+  );
+}
+
+export default App;
