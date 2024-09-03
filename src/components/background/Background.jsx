40deb54003eddc76e64520032acc58e1ecec4ec9
Thomagotchi <thomassano87@gmail.com>
2024-09-03 15:21:42 +0200

    Background + Animation

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,5 +1,12 @@
 import "./background.scss";
 
 export function Background() {
-  return <div className="background"></div>;
+  return (
+    <div className="background">
+      <div className="light-container">
+        <div className="left-light"></div>
+        <div className="right-light"></div>
+      </div>
+    </div>
+  );
 }
