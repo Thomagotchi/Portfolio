2cb2d25244e4d54ad062eefa3e7db2c0b31ccb34
Thomagotchi <thomassano87@gmail.com>
2024-09-13 13:17:58 +0200

    Ajout séparateur de la page realisations

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -7,6 +7,50 @@ export function Projects() {
     <div className="projects-container">
       <div className="projects-year-container">
         <p className="projects-year">2024</p>
+        <svg
+          width="148"
+          height="4"
+          viewBox="0 0 148 4"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+          className="seperator-bar"
+        >
+          <rect
+            width="4"
+            height="4"
+            rx="2"
+            transform="matrix(-1 0 0 1 4 0)"
+            fill="#currentColor"
+          />
+          <rect
+            width="4"
+            height="4"
+            rx="2"
+            transform="matrix(-1 0 0 1 16 0)"
+            fill="#currentColor"
+          />
+          <rect
+            width="4"
+            height="4"
+            rx="2"
+            transform="matrix(-1 0 0 1 28 0)"
+            fill="#currentColor"
+          />
+          <rect
+            width="4"
+            height="4"
+            rx="2"
+            transform="matrix(-1 0 0 1 40 0)"
+            fill="#currentColor"
+          />
+          <rect
+            width="100"
+            height="4"
+            rx="2"
+            transform="matrix(-1 0 0 1 148 0)"
+            fill="#currentColor"
+          />
+        </svg>
       </div>
       <div className="projects-gallery">
         {allProjects.year2024.projects.map((project) => (
