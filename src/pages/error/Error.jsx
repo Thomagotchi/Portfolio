eb951f2fca053cd880cdafa027b183ddc94711b2
Thomagotchi <thomassano87@gmail.com>
2024-09-21 15:26:19 +0200

    Page erreur + SCSS + Responsive

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,3 +1,17 @@
+import "./error.scss";
+import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
 export function Error() {
-  return <h1>Hello Error</h1>;
+  const { t } = useTranslation();
+
+  return (
+    <div className="error-container">
+      <h1 className="error-number">404</h1>
+      <h2>{t("errorPageMessage")}</h2>
+      <NavLink to="/">
+        <p>{t("errorPageLinkMessage")}</p>
+      </NavLink>
+    </div>
+  );
 }
